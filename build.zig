f56913642aa03116736edff729b7c806f6a7d073
frehml <frederic.hamelink@hotmail.com>
2024-11-05 01:24:22 +0100

    fix: remove blocks

--- build.zig
+++ build.zig
@@ -8,42 +8,37 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
     });
 
-    // EXAMPLES
-    basic: {
-        const basic = b.addExecutable(.{
-            .name = "fzwatch-example",
-            .root_source_file = b.path("examples/basic.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        basic.root_module.addImport("fzwatch", module);
-        if (target.result.os.tag == .macos) basic.linkFramework("CoreServices");
-        const run_cmd = b.addRunArtifact(basic);
-        run_cmd.step.dependOn(b.getInstallStep());
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-        const run_step = b.step("run-basic", "Run the example");
-        run_step.dependOn(&run_cmd.step);
-        break :basic;
+    // Basic example
+    const basic = b.addExecutable(.{
+        .name = "fzwatch-example",
+        .root_source_file = b.path("examples/basic.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    basic.root_module.addImport("fzwatch", module);
+    if (target.result.os.tag == .macos) basic.linkFramework("CoreServices");
+    const run_cmd = b.addRunArtifact(basic);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
     }
+    const run_step = b.step("run-basic", "Run the example");
+    run_step.dependOn(&run_cmd.step);
 
-    context: {
-        const context = b.addExecutable(.{
-            .name = "fzwatch-context",
-            .root_source_file = b.path("examples/context.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        context.root_module.addImport("fzwatch", module);
-        if (target.result.os.tag == .macos) context.linkFramework("CoreServices");
-        const run_cmd_context = b.addRunArtifact(context);
-        run_cmd_context.step.dependOn(b.getInstallStep());
-        if (b.args) |args| {
-            run_cmd_context.addArgs(args);
-        }
-        const run_step_context = b.step("run-context", "Run the example");
-        run_step_context.dependOn(&run_cmd_context.step);
-        break :context;
+    // Context example
+    const context = b.addExecutable(.{
+        .name = "fzwatch-context",
+        .root_source_file = b.path("examples/context.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    context.root_module.addImport("fzwatch", module);
+    if (target.result.os.tag == .macos) context.linkFramework("CoreServices");
+    const run_cmd_context = b.addRunArtifact(context);
+    run_cmd_context.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd_context.addArgs(args);
     }
+    const run_step_context = b.step("run-context", "Run the example");
+    run_step_context.dependOn(&run_cmd_context.step);
 }

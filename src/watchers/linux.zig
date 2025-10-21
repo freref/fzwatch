699c143cbe3e1b8e96b660fa87935803cac287e7
freref <35976402+freref@users.noreply.github.com>
2025-10-21 19:45:25 +0200

    fix: offset

--- src/watchers/linux.zig
+++ src/watchers/linux.zig
@@ -1,29 +1,27 @@
 const std = @import("std");
 const interfaces = @import("interfaces.zig");
+const linux = std.os.linux;
 
 pub const LinuxWatcher = struct {
     allocator: std.mem.Allocator,
-    paths: std.ArrayList([]const u8),
-    inotify: struct {
-        fd: i32,
-        /// inotify wd offset from removing files
-        offset: usize,
-    },
+    wd_to_path: std.AutoHashMap(i32, []const u8),
+    path_to_wd: std.StringHashMap(i32),
+    file_count: u32,
+    fd: i32,
     callback: ?*const interfaces.Callback,
     context: ?*anyopaque,
     running: bool,
 
     pub fn init(allocator: std.mem.Allocator) !LinuxWatcher {
-        const fd = try std.posix.inotify_init1(std.os.linux.IN.NONBLOCK);
+        const fd = try std.posix.inotify_init1(linux.IN.NONBLOCK);
         errdefer std.posix.close(fd);
 
         return LinuxWatcher{
             .allocator = allocator,
-            .paths = std.ArrayList([]const u8).empty,
-            .inotify = .{
-                .fd = @intCast(fd),
-                .offset = 1,
-            },
+            .wd_to_path = std.AutoHashMap(i32, []const u8).init(allocator),
+            .path_to_wd = std.StringHashMap(i32).init(allocator),
+            .file_count = 0,
+            .fd = @intCast(fd),
             .callback = null,
             .context = null,
             .running = false,
@@ -32,31 +30,42 @@ pub const LinuxWatcher = struct {
 
     pub fn deinit(self: *LinuxWatcher) void {
         self.stop();
-        self.paths.deinit(self.allocator);
-        std.posix.close(self.inotify.fd);
+        self.wd_to_path.deinit();
+        self.path_to_wd.deinit();
+        std.posix.close(self.fd);
     }
 
     pub fn addFile(self: *LinuxWatcher, path: []const u8) !void {
-        _ = try std.posix.inotify_add_watch(
-            self.inotify.fd,
+        try self._addFile(path);
+        self.file_count += 1;
+    }
+
+    fn _addFile(self: *LinuxWatcher, path: []const u8) !void {
+        const wd = try std.posix.inotify_add_watch(
+            self.fd,
             path,
-            std.os.linux.IN.MODIFY | std.os.linux.IN.CLOSE_WRITE | std.os.linux.IN.ATTRIB | std.os.linux.IN.MOVE_SELF | std.os.linux.IN.DELETE_SELF,
+            linux.IN.MODIFY | linux.IN.CLOSE_WRITE | linux.IN.ATTRIB | linux.IN.MOVE_SELF |
+                linux.IN.DELETE_SELF | linux.IN.IGNORED,
         );
 
-        try self.paths.append(self.allocator, path);
+        try self.wd_to_path.put(wd, path);
+        try self.path_to_wd.put(path, wd);
+        self.file_count += 1;
     }
 
     pub fn removeFile(self: *LinuxWatcher, path: []const u8) !void {
-        for (0.., self.paths.items) |idx, mem_path| {
-            if (!std.mem.eql(u8, mem_path, path))
-                continue;
-
-            _ = std.posix.inotify_rm_watch(self.inotify.fd, @intCast(idx + self.inotify.offset));
-            self.inotify.offset += 1;
-            _ = self.paths.orderedRemove(idx);
-
-            return;
+        if (self.path_to_wd.get(path)) |wd| {
+            _ = std.posix.inotify_rm_watch(self.fd, wd);
+            _ = self.path_to_wd.remove(path);
+            _ = self.wd_to_path.remove(wd);
+            self.file_count -= 1;
         }
+    }
+
+    pub fn getNumberOfFilesBeingWatched(self: *LinuxWatcher) u32 {
+        std.debug.assert(self.file_count == self.wd_to_path.count());
+        std.debug.assert(self.file_count == self.path_to_wd.count());
+        return self.file_count;
     }
 
     pub fn setCallback(
@@ -70,15 +79,15 @@ pub const LinuxWatcher = struct {
 
     pub fn start(self: *LinuxWatcher, opts: interfaces.Opts) !void {
         // TODO add polling instead of busy waiting
-        if (self.paths.items.len == 0) return error.NoFilesToWatch;
+        if (self.file_count == 0) return error.NoFilesToWatch;
 
         self.running = true;
-        var buffer: [4096]std.os.linux.inotify_event = undefined;
+        var buffer: [65536]u8 = undefined;
 
         while (self.running) {
             const length = std.posix.read(
-                self.inotify.fd,
-                std.mem.sliceAsBytes(&buffer),
+                self.fd,
+                &buffer,
             ) catch |err| switch (err) {
                 error.WouldBlock => {
                     std.Thread.sleep(@as(u64, @intFromFloat(@as(f64, opts.latency) * @as(
@@ -94,21 +103,29 @@ pub const LinuxWatcher = struct {
 
             // in bytes
             var i: usize = 0;
-            while (i < length) : (i += buffer[i].len + @sizeOf(std.os.linux.inotify_event)) {
-                const ev = buffer[i];
+            while (i < length) {
+                const ev_ptr: *align(1) linux.inotify_event =
+                    @ptrCast(buffer[i..][0..@sizeOf(linux.inotify_event)].ptr);
 
-                if (ev.wd < self.inotify.offset) {
-                    continue;
-                } else if (ev.wd > self.paths.items.len + self.inotify.offset)
-                    return error.InvalidWatchDescriptor;
+                const ev = ev_ptr.*;
+                const step = @sizeOf(linux.inotify_event) + ev.len;
 
-                const index = @as(usize, @intCast(@max(0, ev.wd))) - self.inotify.offset;
+                const rec_size = @sizeOf(linux.inotify_event) + ev.len;
+                if (i + rec_size > length) break;
+
+                const path = self.wd_to_path.get(ev.wd) orelse break;
+
                 // Editors like vim create temporary files when saving
                 // So we have to re-add the file to the watcher
-                if (ev.mask & (std.os.linux.IN.DELETE_SELF | std.os.linux.IN.MOVE_SELF | std.os.linux.IN.IGNORED) != 0) {
-                    try self.addFile(self.paths.items[index]);
+                if (ev.mask & (linux.IN.DELETE_SELF | linux.IN.MOVE_SELF |
+                    linux.IN.IGNORED) != 0)
+                {
+                    try self._addFile(path);
                 }
+
                 if (self.callback) |callback| callback(self.context, .modified);
+
+                i += step;
             }
         }
     }
